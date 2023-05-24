00617e2eef8212001035b9602180f2167ab77da5
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-05-24 12:17:45 +0300

    Implementation of the bubble sort

---
+++ ex_proj/bubblesort.c
@@ -0,0 +1,38 @@
+# include <stdio.h>
+# include <string.h>
+
+void bubble();
+
+/* sort driver */
+
+int main() /* sort a string from a keyboard */
+{
+	char s[1024];
+
+	printf("Enter a string: ");
+	scanf("%s", s);
+	bubble(s, strlen(s));
+	printf("The sorted string is: %s\n", s);
+
+	return (0);
+}
+
+/* The bubble sort */
+
+void bubble(item, count)
+char *item;
+int count;
+{
+	register int a, b;
+	register char t;
+
+	for (a = 1; a < count; ++a)
+		for (b = count - 1; b >= a; --b){
+			if (item[b - 1] > item[b]){
+				/* swap elements*/
+				t = item[b - 1];
+				item[b - 1] = item[b];
+				item[b] = t;
+			}
+		}
+}
